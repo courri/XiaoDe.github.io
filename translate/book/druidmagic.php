6a63dbb7b0267eb1740bf4d36d2326928291a387
default_git_name <default_git_email>
2014-01-28 10:09:51 +0800

    完善《德鲁伊魔法》；修改自动导出脚本

--- translate/book/druidmagic.php
+++ translate/book/druidmagic.php
@@ -1,30 +1,44 @@
 <?php
+/**
+* kindle笔记导出成html
+*/
+
+/* 欲导出电子书id */
+$bookId = 'AMZNID0/B00352M8LE/0/';
+
+/* 导出目录 */
+$exportPath = '/sdcard/大地行者/小德/translate/book';
+
+/* 导出html文件名 */
+$htmlName = 'druidmagic.zhcn.html';
+
+/* 复制笔记数据库文件名 */
+$dbName = 'druidmagic.db';
+
+/* 复制php文件名 */
+$phpName = 'druidmagic.php';
+
+/* kindle笔记数据库路径 */
+$noteDbPath = '/data/data/com.amazon.kindle/databases/annotations.db';
+
+/* 以下无需修改 */
 header("Content-Type: text/html; charset=utf-8");
-$txt = '/sdcard/大地行者/小德/translate/book/druidmagic.zhcn.html';
-$sqlite = '/data/data/com.amazon.kindle/databases/annotations.db';
-copy($sqlite, dirname($txt).'/druidmagic.db');
-copy(__FILE__, dirname($txt).'/druidmagic.php');
-$fp = fopen($txt, 'w');
-fwrite($fp, $css = <<<HTML
-<!doctype html>
-<meta charset="utf-8">
-<style type="text/css">
-  p {text-indent:2em}
-  p.quote {font-style:italic}
-</style>
+//复制php和db文件
+copy($noteDbPath, "$exportPath/$dbName");
+copy(__FILE__, "$exportPath/$phpName");
+$fp = fopen("$exportPath/$htmlName", 'w');
 
-HTML
-);
+$db = new pdo("sqlite:$noteDbPath");
 
-echo $css;
-
-$db = new pdo('sqlite:'.$sqlite);
-
-$sql = 'select USER_TEXT from Annotations WHERE TYPE=1 AND BOOKID=\'AMZNID0/B00352M8LE/0/\' ORDER BY START_POS ASC';
+$sql = "SELECT USER_TEXT FROM Annotations WHERE TYPE=1 AND BOOKID='$bookId' ORDER BY START_POS ASC";
 $rs = $db->query($sql);
 while(false !== $data=$rs->fetch(PDO::FETCH_NUM))
 {
     echo $data[0];
     fwrite($fp, $data[0]);
+    if (preg_match('!</\w+>$!s', $data[0])) {
+        echo "\n";
+        fwrite($fp, "\n");
+    }
 }
 fclose($fp);
